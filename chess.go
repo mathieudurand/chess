5970cd563a48b5448d2c6d0c5f9fc85dc5154ac3
Mathieu Durand <mathieudurand79@gmail.com>
2017-05-10 21:49:40 -0400

    adding todo in comments

--- chess.go
+++ chess.go
@@ -6,6 +6,16 @@ import (
 	"io/ioutil"
 )
 
+// build class for board
+// class will be used to "setup" the move list requested and propose a move to follow with
+
+    // func validate move list is valid
+
+    // func needed to check if move valid
+
+
+// func needed to evaluate total value capture pieces in a branch of moves
+
 // main loop function
 func launch(moves string){
     fmt.Print(moves)
@@ -20,11 +30,13 @@ func check(e error) {
 // argument processing and sourcing move list
 func main() {
 
-    boolPtr := flag.Bool("verbose", false, "a bool")
+    boolPtr := flag.Bool("verbose", false, "Verbose mode")
+    movesAheadPtr := flag.Int("movesAhead", 3, "Number of moves to think ahead on.")
     fileStringPtr := flag.String("filename","moves.txt","A file with a list of chess moves in it")
     flag.Parse()
 
     fmt.Println("Verbose mode: ", *boolPtr)
+    fmt.Println("movesAhead: ", *movesAheadPtr)
     fmt.Println("File with moves: ", *fileStringPtr)
 
     dat, err := ioutil.ReadFile("./moves.txt")
